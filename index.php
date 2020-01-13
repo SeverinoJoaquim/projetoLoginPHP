57918b20c53a9cf274a99cb9a56ed2a9079c8b92
SeverinoJoaquim <severinojoca@gmail.com>
2020-01-13 17:22:03 -0300

    02-Front-end parte 2

--- index.php
+++ index.php
@@ -3,7 +3,12 @@
 
 <head>
 	<title></title>
+
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
@@ -15,9 +20,13 @@
 		<h1>Faça seu login</h1>
 
 		<form>
-			<input type="email" name="e-mail" placeholder="exemple@exemple.com.br">
-			<input type="password" name="password" placeholder="*********">
+			<input type="email" name="e-mail" placeholder="E-MAIL">
+			<input type="password" name="password" placeholder="SENHA">
+
 			<button class="btn-login">LOGAR</button>
+			<span class="msg_error"><i class="fa fa-exclamation-circle" style="font-size: 18px; color: #ff6d6d; padding-right: 5px;"></i>
+				<p>Tentativa inválida!</p>
+			</span>
 		</form>
 	</div>
 
