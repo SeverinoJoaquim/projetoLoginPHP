6388a8ba8125dcd1494b92fc10f2c06101354912
SeverinoJoaquim <severinojoca@gmail.com>
2020-01-13 16:09:50 -0300

    Aula 01 - Front-end parte 1

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+
+<body>
+
+	<div class="box_login">
+		<div class="capa">
+
+		</div>
+		<h1>Faça seu login</h1>
+
+		<form>
+			<input type="email" name="e-mail" placeholder="exemple@exemple.com.br">
+			<input type="password" name="password" placeholder="*********">
+			<button class="btn-login">LOGAR</button>
+		</form>
+	</div>
+
+</body>
+
+</html>
